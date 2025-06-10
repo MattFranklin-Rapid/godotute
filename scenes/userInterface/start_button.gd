753d723a1da38e860df8105c067fdb45f435bf7c
Matthew Franklin <matthew@rapidplatform.com.au>
2025-06-10 18:04:34 +1000

    Release - start at test level 2

--- scenes/userInterface/start_button.gd
+++ scenes/userInterface/start_button.gd
@@ -6,4 +6,5 @@ func _ready():
 	start_button.pressed.connect(_button_pressed)
 
 func _button_pressed():
-	get_tree().change_scene_to_file("uid://bs51eubaxo8ha") #Test Level
+	#get_tree().change_scene_to_file("uid://bs51eubaxo8ha") #Test Level
+	get_tree().change_scene_to_file("uid://c34akfbxthxqd") #Test Level 2
